fb469b6e6bb9c31bda09f309c0bab1339a5416ad
Akash <akbm95326@gmail.com>
2025-09-01 18:55:34 +0530

    added counter app project

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,12 @@
+import Chai from "./chai";
+function App() {
+  return (
+    <>
+      <Chai />
+      <h1>Chai aur react</h1>
+      <p>Akash bhai kaise ho</p>
+    </>
+  );
+}
+
+export default App;
